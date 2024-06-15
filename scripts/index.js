241e61337cf960d0e99a5586f37c289cf8fb3f86
Roman Demianchuk <roman.g.demyanchuk@gmail.com>
2024-06-15 15:25:11 +0300

    Add tiles view, add switcher for rows and tiles

--- scripts/index.js
+++ scripts/index.js
@@ -1,16 +1,30 @@
-const picker1 = datepicker('.first-calendar .date', {
-    language: "en"
+document.addEventListener("DOMContentLoaded", function() {
+    datepicker('.first-calendar .date', {
+        language: "en"
+    });
+
+    datepicker('.second-calendar .date', {
+        language: "en"
+    });
+
+    let tilesIconElement = document.querySelector('.tiles-icon');
+    let rowsIconElement = document.querySelector('.rows-icon');
+    let rowsSection = document.querySelector('.rows-section');
+    let tilesSection = document.querySelector('.tiles-section');
+
+    document.body.addEventListener('click', function( event ){
+        if(tilesIconElement.contains(event.target)){
+            rowsSection.style.display = 'none';
+            tilesSection.style.display = 'block';
+            tilesIconElement.classList.add('active');
+            rowsIconElement.classList.remove('active')
+        }
+
+        if(rowsIconElement.contains(event.target)){
+            rowsSection.style.display = 'block';
+            tilesSection.style.display = 'none';
+            tilesIconElement.classList.remove('active');
+            rowsIconElement.classList.add('active')
+        }
+    });
 });
-
-const picker2 = datepicker('.second-calendar .date', {
-    language: "en"
-});
-
-button.addEventListener('click', e => {
-    // THIS!!! Prevent Datepicker's event handler from hiding the calendar.
-    e.stopPropagation()
-
-    // Toggle the calendar.
-    const isHidden = picker.calendarContainer.classList.contains('qs-hidden')
-    picker[isHidden ? 'show' : 'hide']()
-})
